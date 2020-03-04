1ddaf79bcd270c9ac5ece8167243f62ee0760f02
Jonathan Wall <jonathantwall@gmail.com>
2020-03-04 01:11:17 -0800

    cp

--- src/main/java/com/example/cmdline/Runner.java
+++ src/main/java/com/example/cmdline/Runner.java
@@ -3,10 +3,14 @@ package com.example.cmdline;
 import com.example.Greeting;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
+import dagger.Component;
+import dagger.Module;
+import dagger.Provides;
 
 import javax.annotation.Nullable;
+import javax.inject.Inject;
+import javax.inject.Singleton;
 import java.util.List;
-// import javax.inject.Inject;
 
 public class Runner {
   public static void main(String args[]) {
@@ -17,10 +21,27 @@ public class Runner {
   }
 
 
-  // @Module
-      // @Inject
-  class AppModule {
-    // ...
+  @Module
+  static class ToyModule {
+    @Provides
+    @Singleton
+    static Greeting provideGreeting() {
+      return new Greeting();
+    }
+  }
 
+  static class ToyApp {
+    public final Greeting greeting;
+
+    @Inject
+    ToyApp(Greeting greeting) {
+      this.greeting = greeting;
+    }
+  }
+
+  @Singleton
+  @Component(modules = {ToyModule.class})
+  interface ToyComponent {
+    ToyApp toyApp();
   }
 }
