570d5a9963cc084b0533b25f17b1677398aae5eb
Jonathan Wall <jonathantwall@gmail.com>
2020-03-04 00:57:41 -0800

    cp

--- src/main/java/com/example/cmdline/Runner.java
+++ src/main/java/com/example/cmdline/Runner.java
@@ -1,9 +1,24 @@
 package com.example.cmdline;
 
 import com.example.Greeting;
+import com.google.common.collect.Lists;
+import java.util.List;
 
-   public class Runner {
-       public static void main(String args[]) {
-           Greeting.sayHi();
-   }
+import javax.annotation.Nullable;
+// import javax.inject.Inject;
+
+public class Runner {
+  public static void main(String args[]) {
+    @Nullable Integer foo = null;
+    List<String> bar = Lists.newArrayList();
+    Greeting.sayHi();
+  }
+
+
+  // @Module
+      // @Inject
+  class AppModule {
+    // ...
+
+  }
 }
